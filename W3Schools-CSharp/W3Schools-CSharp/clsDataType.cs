088091438339be6770de2687cbbc70845248f249
yladev <yanlinnaung881@gmail.com>
2024-01-29 23:55:28 +0630

    update update dataType

---
+++ W3Schools-CSharp/W3Schools-CSharp/clsDataType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W3Schools_CSharp
+{
+    class clsDataType
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 2 * x + 1;
+            for (int x = 0; x <= 10 ; x++)
+            {
+                Console.WriteLine("2 * " + x + "+1=" + y);   
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
