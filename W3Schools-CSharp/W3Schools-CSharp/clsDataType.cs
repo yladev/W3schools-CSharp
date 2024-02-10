da120a54a6fe2043f3df69b954b6809a076e340f
yladev <yanlinnaung881@gmail.com>
2024-02-10 22:09:01 +0630

    update datatype

--- W3Schools-CSharp/W3Schools-CSharp/clsDataType.cs
+++ W3Schools-CSharp/W3Schools-CSharp/clsDataType.cs
@@ -10,13 +10,29 @@ namespace W3Schools_CSharp
     {
         static void Main(string[] args)
         {
-            int x = 1;
-            int y = 2 * x + 1;
-            for (int x = 0; x <= 10 ; x++)
-            {
-                Console.WriteLine("2 * " + x + "+1=" + y);   
-            }
+            int numberOne = 10;
+            Console.WriteLine("numberOne = " + numberOne);
 
+            long petrolLiter = 15000000000L;
+            Console.WriteLine("petrolLiter = " + petrolLiter);
+
+            float myNum = 5.75F;
+            Console.WriteLine("myNum = " + myNum);
+
+            double myNumber = 19.99D;
+            Console.WriteLine("myNumber = " + myNumber);
+
+            bool isTrue = true;
+            Console.WriteLine("isTrue = " + isTrue);
+
+            bool isFalse = false;
+            Console.WriteLine("isFalse = " + isFalse);
+
+            char myGrade = 'A';
+            Console.WriteLine("myGrade = " + myGrade);
+
+            string greeting = "Mingalarpr";
+            Console.WriteLine("greeting = " + greeting);
 
             Console.ReadKey();
         }
